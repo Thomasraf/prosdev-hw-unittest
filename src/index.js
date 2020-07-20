43afa28432524456cf7b8b0b6251d24e02b3e9d3
Thomasraf <thomas_cruz@dlsu.edu.ph>
2020-07-20 21:00:32 +0800

    done with unit testing

--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
 const functions = {
   gradeClassifier: (grade) => {
-    // TODO: Write code here
+    if(grade>=95 && grade<=100){
+      var string = "O Outstanding"
+      return string
+    }
+    else if(grade>=90 && grade<=94.99){
+      return "V Very Good"
+    }
+    else if(grade>=85 && grade<=89.99){
+      return "G Good"
+    }
+    else if(grade>=80 && grade<=84.99){
+      return "S Satisfactory"
+    }
+    else if(grade>=75 && grade<=79.99){
+      return "N Needs Improvement"
+    }
+    else if(grade<= 74.99){
+      return "D Did not Meet Expectation"
+    }
   }
 }
 
--- src/index.test.js
+++ src/index.test.js
@@ -2,11 +2,63 @@ const { gradeClassifier } = require('./index');
 
 // TODO: Write unit tests here
 describe('Scenario', () => {
-  it('When ..., then ...', () => {
+  it('When grade is 98, then it should be Outstanding!', () => {
     // Arrange
-
+    var data = 98;
+    var expected ='O Outstanding';
     // Act
-
+    var result = gradeClassifier(data);
     // Assert
+    expect(result).toBe(expected);
+  });
+
+  it('When grade is 93, then it should be Very Good!', () => {
+    // Arrange
+    var data = 93;
+    var expected ='V Very Good';
+    // Act
+    var result = gradeClassifier(data);
+    // Assert
+    expect(result).toBe(expected);
+  });
+
+  it('When grade is 87, then it should be Good!', () => {
+    // Arrange
+    var data = 87;
+    var expected ='G Good';
+    // Act
+    var result = gradeClassifier(data);
+    // Assert
+    expect(result).toBe(expected);
+  });
+
+  it('When grade is 82, then it should be Satisfactory!', () => {
+    // Arrange
+    var data = 82;
+    var expected ='S Satisfactory';
+    // Act
+    var result = gradeClassifier(data);
+    // Assert
+    expect(result).toBe(expected);
+  });
+
+  it('When grade is 77, then it should be Needs Improvement!', () => {
+    // Arrange
+    var data = 77;
+    var expected ='N Needs Improvement';
+    // Act
+    var result = gradeClassifier(data);
+    // Assert
+    expect(result).toBe(expected);
+  });
+
+  it('When grade is 60, then it should be Did not Meet Expectation!', () => {
+    // Arrange
+    var data = 60;
+    var expected ='D Did not Meet Expectation';
+    // Act
+    var result = gradeClassifier(data);
+    // Assert
+    expect(result).toBe(expected);
   });
 });
